4cab8923050b7fa109f82e50623adc5f3b9f4b72
Josh <jozsef.bone@gmail.com>
2025-08-30 21:41:36 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,51 @@
-module.exports = () => ({});
+module.exports = () => ({
+    meilisearch: {
+        config: {
+            "activism-story": {
+                indexName: "labouractivism",
+                transformEntry({ entry }) {
+                    return {
+                        ...entry,
+                        type: 'activismStory'
+                    }
+                },
+            }, 
+            "essay": {
+                indexName: "labouractivism",
+                transformEntry({ entry }) {
+                    return {
+                        ...entry,
+                        type: 'essay'
+                    }
+                },
+            }, 
+            "network": {
+                indexName: "labouractivism",
+                transformEntry({ entry }) {
+                    return {
+                        ...entry,
+                        type: 'network'
+                    }
+                },
+            },
+            "person": {
+                indexName: "labouractivism",
+                transformEntry({ entry }) {
+                    return {
+                        ...entry,
+                        type: 'person'
+                    }
+                },
+            },
+            "source": {
+                indexName: "labouractivism",
+                transformEntry({ entry }) {
+                    return {
+                        ...entry,
+                        type: 'source'
+                    }
+                },
+            },
+        }
+    }
+});
