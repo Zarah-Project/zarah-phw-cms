a257e4ab14b7d8721d4e2be1e5f31d50eec43e3a
Josh <jozsef.bone@gmail.com>
2025-08-31 21:02:27 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -4,36 +4,56 @@ module.exports = () => ({
             "activism-story": {
                 indexName: "labouractivism",
                 transformEntry({ entry }) {
+                    const transformedEntry = entry
+                    delete transformedEntry.Essyas
+                    delete transformedEntry.Networks
+                    delete transformedEntry.People
                     return {
-                        ...entry,
-                        type: 'activismStory'
+                        ...transformedEntry,      
+                        type: 'activismStory',
+                        Tags: transformedEntry.Tags.map(tag => tag.name),
                     }
                 },
             }, 
             "essay": {
                 indexName: "labouractivism",
                 transformEntry({ entry }) {
+                    const transformedEntry = entry
+                    delete transformedEntry.ActivismStories
+                    delete transformedEntry.Networks
+                    delete transformedEntry.People
                     return {
-                        ...entry,
-                        type: 'essay'
+                        ...transformedEntry,      
+                        type: 'essay',
+                        Tags: transformedEntry.Tags.map(tag => tag.name),
                     }
                 },
             }, 
             "network": {
                 indexName: "labouractivism",
                 transformEntry({ entry }) {
+                    const transformedEntry = entry
+                    delete transformedEntry.ActivismStories
+                    delete transformedEntry.Essyas
+                    delete transformedEntry.People
                     return {
-                        ...entry,
-                        type: 'network'
+                        ...transformedEntry,      
+                        type: 'network',
+                        Tags: transformedEntry.Tags.map(tag => tag.name),
                     }
                 },
             },
             "person": {
                 indexName: "labouractivism",
                 transformEntry({ entry }) {
+                    const transformedEntry = entry
+                    delete transformedEntry.ActivismStories
+                    delete transformedEntry.Essyas
+                    delete transformedEntry.Networks
                     return {
-                        ...entry,
-                        type: 'person'
+                        ...transformedEntry,      
+                        type: 'person',
+                        Tags: transformedEntry.Tags.map(tag => tag.name),
                     }
                 },
             },
@@ -42,7 +62,8 @@ module.exports = () => ({
                 transformEntry({ entry }) {
                     return {
                         ...entry,
-                        type: 'source'
+                        type: 'source',
+                        Tags: entry.Tags.map(tag => tag.name),
                     }
                 },
             },
